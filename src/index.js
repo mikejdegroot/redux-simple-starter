aa2b86044342b4a7d488f9bdd9075652e3d9919b
Mike de Groot <mike.j.degroot@gmail.com>
2017-10-09 13:45:09 +0100

    first compenant rendered \!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
 // create a new component. this component should produce some html
 
+const App = function() { //this function is a class ie it constructs instances of what is inside the func
+  return <div> Hi! </div>;
+}
+
 // insert the component's generated html and put it in the dom
+
+ReactDOM.render(<App />, document.querySelector('.container')); //an instance of the class must be passed to the dom, not the class itself
+//that is what a self closing jsx tag looks like
+//the second argument can be thought of as a target destination for the render
